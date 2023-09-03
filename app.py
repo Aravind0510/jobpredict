4355fc64dad1cfb7bd81cfdf26d933ca7948b772
Aravind0510 <106546654+Aravind0510@users.noreply.github.com>
2023-09-03 12:12:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import numpy as np
+from flask import Flask,request,jsonify,render_template
+import pickle
+
+app=Flask(__name__)
+model=pickle.load(open('random.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index1.html')  #html page here
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    int_features=[[int(x) for x in request.form.values()]]
+    print(int_features)
+    final_features=np.array(int_features)  
+    prediction=model.predict(final_features)
+    prediction=prediction.tolist()
+    
+    data = {
+            "prediction" : prediction
+        }
+    return jsonify(data)
+    
+    # return render_template('index.html',prediction_text='Loan eligiblity is {}'.format(output))
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
